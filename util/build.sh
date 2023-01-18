68ec3686a493ea8acb2e796f371752adc0beaecf
ariyn <himnowxz@gmail.com>
2023-01-18 22:57:58 +0900

    update build script
    remove unused clock code

--- util/build.sh
+++ util/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-go build -o bin/not ./not
-go build -o bin/or ./or
-go build -o bin/and ./and
-go build -o bin/xor ./xor
-go build -o "bin/alias" "./alias"
-go build -o bin/clock ./clock
-go build -o bin/flipflop ./flipflop
-go build -o bin/input ./input
+go build -o bin/not ./app/not
+go build -o bin/or ./app/or
+go build -o bin/and ./app/and
+go build -o bin/xor ./app/xor
+go build -o "bin/alias" "./app/alias"
+go build -o bin/flipflop ./app/flipflop
+go build -o bin/input ./app/input
 go build -o bin/parser ./parser
