b0ebc1fc8fc3e6903b9a649d79f7f8516e4d4eb0
ariyn <himnowxz@gmail.com>
2023-01-18 21:10:23 +0900

    and gate restructuring

---
+++ app/and/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	cc "github.com/ariyn/cloud-computer"
+	"github.com/ariyn/cloud-computer/gate"
+	"log"
+)
+
+func main() {
+	name := cc.Name
+	if name == "" {
+		name = "and"
+	}
+	inputs := cc.ParseInputs(cc.Inputs...)
+	outputs := cc.CreateOutputs(1)
+
+	if cc.IsVerbose {
+		log.Println(inputs)
+	}
+
+	or := gate.NewAndGate(name, inputs, outputs)
+	or.UseOptimization = cc.UseOptimization
+
+	err := cc.RunGateWithRedis(context.Background(), or)
+	if err != nil {
+		panic(err)
+	}
+}
